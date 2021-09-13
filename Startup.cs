1556904db1f769d6f5d1404241a963b044664cd5
Goblin school <goblinschool2017@outlook.com>
2021-09-13 16:21:21 +0530

    commit5

--- Startup.cs
+++ Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+//commit-5
 namespace myapp
 {
     public class Startup
