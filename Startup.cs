0ebb9c0da198283f9ddce0ae2977f9667df022d0
Goblin school <goblinschool2017@outlook.com>
2021-09-13 17:26:25 +0530

    commit 6 in feature branch in startup

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 //commit-5
+//commit-6 in feature branch
 namespace myapp
 {
     public class Startup
